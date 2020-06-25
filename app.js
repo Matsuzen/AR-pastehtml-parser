bea71fbb27dde8db86441c44070f5d3b5baa9ac4
jrenaldev <jrenald0@hotmail.com>
2020-06-25 17:50:26 -0400

    Fixed unused test vars in app.js

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models/db");
 
-const dbRoutes = require("./controllers/db/manipRoutes");
-
 const readPastehtmls = require("./funcs/readPastehtmls");
-const storePasteInfo = require("./funcs/storePasteInfo");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.use("/db", dbRoutes);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
