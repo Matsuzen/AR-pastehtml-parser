397e186d67ced2112cbd6873d56be4e5e5df415b
jrenaldev <jrenald0@hotmail.com>
2021-06-22 18:15:05 -0400

    add: initiate bot

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ const db = require("./models/db");
 
 const router = require("./routes");
 
-const readPastehtmls = require("./funcs/readPastehtmls");
-const exportStats = require("./funcs/exportStats");
-const updateSpreadsheet = require("./funcs/updateSpreadsheet");
+// const readPastehtmls = require("./funcs/readPastehtmls");
+// const exportStats = require("./funcs/exportStats");
+// const updateSpreadsheet = require("./funcs/updateSpreadsheet");
+// const findTeamsFromSheet = require("./funcs/findTeamsFromSheet");
+// const insertTeams = require("./funcs/insertTeams");
+// const findMatch = require("./funcs/findMatch");
+// const parseHtml = require("./funcs/parseHtml");
+
+const initDiscordBot = require("./funcs/discord/initDiscordBot")();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
@@ -19,7 +25,3 @@ app.use(express.json());
 app.use("/db", router);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-/* updateSpreadsheet(); */
-/* exportStats("Libero"); */
-/* readPastehtmls(); */
