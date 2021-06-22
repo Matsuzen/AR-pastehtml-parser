dca44c44af0ff058ae3668698e9474345b83fcfe
jrenaldev <jrenald0@hotmail.com>
2021-06-22 17:49:38 -0400

    fix: add tnt kills object to the insertStats function

--- funcs/storePasteInfo.js
+++ funcs/storePasteInfo.js
@@ -4,19 +4,40 @@ const insertStats = require("./insertStats");
 const insertWoolTouches = require("./insertWoolTouches");
 
 //Tournament is optional
-async function storePasteInfo(url, tournament) {
+async function storePasteInfo(url, tournament, i) {
   try {
-    const { matchDetails, teams, woolTouches, playersStats } = await parseHtml(url);
-  
+    const parsedHtmlRes = await parseHtml(url, i);
+    
+    if(parsedHtmlRes.err) {
+      return parsedHtmlRes;
+    }
+
+    const { matchDetails, teams, woolTouches, tntKills, playersStats } = parsedHtmlRes;
+
+    //Both teams need to have 4 members to insert match stats
+    /* if(teams[0].members.length !== 4 || teams[1].members.length !== 4) {
+      return {
+        err: true,
+        message: "Teams are not full, can not insert stats",
+      }
+    } */
+
     const { newMatchId, tournamentId } = await insertMatch(matchDetails, teams, tournament);
   
-    const newStats = await insertStats(playersStats, woolTouches, newMatchId, tournamentId);
+    const newStats = await insertStats(playersStats, woolTouches, tntKills, newMatchId, tournamentId);
 
-    const newWoolTouches = await insertWoolTouches(woolTouches, newMatchId);
+    const newWoolTouches = await insertWoolTouches(woolTouches, newMatchId)
+    
+    return {
+      message: "Succesfully stored pastehtml's info"
+    }
 
   }
   catch(e) {
-    console.log(e);
+    return {
+      err: true,
+      message: `Could not store pastehtml's info: ${e}`
+    }
   }
 
 
